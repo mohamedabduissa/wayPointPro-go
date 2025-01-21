7482b3a70c8f47f42eabcea9c06c0ea611e23970
Mohamed abdu <42504026+mohamedabduissa@users.noreply.github.com>
2025-01-21 10:29:05 +0200

    first

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # wayPointPro-go
+
+wget https://go.dev/dl/go1.23.5.linux-amd64.tar.gz
+
+sudo tar -C /usr/local -xzf go1.23.5.linux-amd64.tar.gz
+
+echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
+source ~/.bashrc
+
+git clone https://github.com/mohamedabduissa/wayPointPro-go.git
+
+cd wayPointPro-go
+
+go mod tidy
+
+go run cmd/main.go
+
--- internal/config/config.go
+++ internal/config/config.go
@@ -3,24 +3,44 @@ package config
 import (
 	"log"
 	"os"
+	"sync"
 
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	OSRMHost string
-	Port     string
+	OSRMHost   string
+	Port       string
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
 }
 
-func LoadConfig() *Config {
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found, using defaults")
-	}
+var (
+	instance *Config
+	once     sync.Once
+)
 
-	return &Config{
-		OSRMHost: getEnv("OSRM_HOST", "http://207.127.98.226:5000"),
-		Port:     getEnv("PORT", "8080"),
-	}
+func LoadConfig() *Config {
+	once.Do(func() {
+		if err := godotenv.Load(); err != nil {
+			log.Println("No .env file found, using defaults")
+		}
+
+		config := &Config{
+			OSRMHost:   getEnv("OSRM_HOST", "http://207.127.98.226:5000"),
+			Port:       getEnv("PORT", "8080"),
+			DBHost:     getEnv("DB_HOST", "localhost"),
+			DBUser:     getEnv("DB_USERNAME", "postgres"),
+			DBPassword: getEnv("DB_PASSWORD", "123456"),
+			DBName:     getEnv("DB_NAME", "waypointpro_1"),
+			DBPort:     getEnv("DB_PORT", "5432"),
+		}
+		instance = config
+	})
+	return instance
 }
 
 func getEnv(key, fallback string) string {
