e4bb0fef0f729efd3f7a4cf7183aedeb5208e5f5
Mohamed abdu <42504026+mohamedabduissa@users.noreply.github.com>
2025-06-16 21:42:02 +0300

    Update gecoding_api.go

--- internal/handlers/map_service/gecoding_api.go
+++ internal/handlers/map_service/gecoding_api.go
@@ -95,26 +95,27 @@ func GetGeCodingHandler(c *gin.Context) {
 		gecoderService.Cache.RedisClient.FlushDB(gecoderService.Cache.CTX)
 
 		cachedKeys := []string{
-			gecoderService.Cache.GenerateGecodeCacheKey("airport_google", lat, lng, "SA", "en", 10),
-			gecoderService.Cache.GenerateGecodeCacheKey("airport_google", lat, lng, "KW", "en", 10),
-			gecoderService.Cache.GenerateGecodeCacheKey("airport_google", lat, lng, "EG", "en", 10),
-			gecoderService.Cache.GenerateGecodeCacheKey("airport_google", lat, lng, "KW", "ar", 10),
-			gecoderService.Cache.GenerateGecodeCacheKey("airport_google", lat, lng, "SA", "ar", 10),
-			gecoderService.Cache.GenerateGecodeCacheKey("airport_google", lat, lng, "EG", "ar", 10),
-
-			gecoderService.Cache.GenerateGecodeCacheKey("mall_google", lat, lng, "SA", "en", 10),
-			gecoderService.Cache.GenerateGecodeCacheKey("mall_google", lat, lng, "KW", "en", 10),
-			gecoderService.Cache.GenerateGecodeCacheKey("mall_google", lat, lng, "EG", "en", 10),
-			gecoderService.Cache.GenerateGecodeCacheKey("mall_google", lat, lng, "KW", "ar", 10),
-			gecoderService.Cache.GenerateGecodeCacheKey("mall_google", lat, lng, "SA", "ar", 10),
-			gecoderService.Cache.GenerateGecodeCacheKey("mall_google", lat, lng, "EG", "ar", 10),
-
-			gecoderService.Cache.GenerateGecodeCacheKey("tourist attraction_google", lat, lng, "SA", "en", 10),
-			gecoderService.Cache.GenerateGecodeCacheKey("tourist attraction_google", lat, lng, "KW", "en", 10),
-			gecoderService.Cache.GenerateGecodeCacheKey("tourist attraction_google", lat, lng, "EG", "en", 10),
-			gecoderService.Cache.GenerateGecodeCacheKey("tourist attraction_google", lat, lng, "KW", "ar", 10),
-			gecoderService.Cache.GenerateGecodeCacheKey("tourist attraction_google", lat, lng, "SA", "ar", 10),
-			gecoderService.Cache.GenerateGecodeCacheKey("tourist attraction_google", lat, lng, "EG", "ar", 10),
+			//gecoderService.Cache.GenerateGecodeCacheKey("airport_google", lat, lng, "SA", "en", 10),
+			//gecoderService.Cache.GenerateGecodeCacheKey("airport_google", lat, lng, "KW", "en", 10),
+			//gecoderService.Cache.GenerateGecodeCacheKey("airport_google", lat, lng, "EG", "en", 10),
+			//gecoderService.Cache.GenerateGecodeCacheKey("airport_google", lat, lng, "KW", "ar", 10),
+			//gecoderService.Cache.GenerateGecodeCacheKey("airport_google", lat, lng, "SA", "ar", 10),
+			//gecoderService.Cache.GenerateGecodeCacheKey("airport_google", lat, lng, "EG", "ar", 10),
+			//
+			//gecoderService.Cache.GenerateGecodeCacheKey("mall_google", lat, lng, "SA", "en", 10),
+			//gecoderService.Cache.GenerateGecodeCacheKey("mall_google", lat, lng, "KW", "en", 10),
+			//gecoderService.Cache.GenerateGecodeCacheKey("mall_google", lat, lng, "EG", "en", 10),
+			//gecoderService.Cache.GenerateGecodeCacheKey("mall_google", lat, lng, "KW", "ar", 10),
+			//gecoderService.Cache.GenerateGecodeCacheKey("mall_google", lat, lng, "SA", "ar", 10),
+			//gecoderService.Cache.GenerateGecodeCacheKey("mall_google", lat, lng, "EG", "ar", 10),
+			//
+			//gecoderService.Cache.GenerateGecodeCacheKey("tourist attraction_google", lat, lng, "SA", "en", 10),
+			//gecoderService.Cache.GenerateGecodeCacheKey("tourist attraction_google", lat, lng, "KW", "en", 10),
+			//gecoderService.Cache.GenerateGecodeCacheKey("tourist attraction_google", lat, lng, "EG", "en", 10),
+			//gecoderService.Cache.GenerateGecodeCacheKey("tourist attraction_google", lat, lng, "KW", "ar", 10),
+			//gecoderService.Cache.GenerateGecodeCacheKey("tourist attraction_google", lat, lng, "SA", "ar", 10),
+			//gecoderService.Cache.GenerateGecodeCacheKey("tourist attraction_google", lat, lng, "EG", "ar", 10),
+			"e7e2516cea6859b5c9d63a366feb943e5354bf10f85e5b341a4025ae22bb0f02",
 		}
 
 		// Build placeholder string: $1, $2, ..., $n
